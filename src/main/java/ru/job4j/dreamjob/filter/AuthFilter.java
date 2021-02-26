bfbf3ebb5a7ff6786b9d0f07fa89344774f78ee1
grigan-Uragan <prischepny@mail.ru>
2021-02-26 23:40:21 +0300

    2. Filter [#469304]

---
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -0,0 +1,35 @@
+package ru.job4j.dreamjob.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String uri = request.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
